d034d993a825e8073a3de01f8217b9e4b5ed05ea
AJ Cody <ajcody13@gmail.com>
2021-10-01 16:27:50 -0400

    First commit to GitHub

---
+++ vue/src/views/UpdateBeerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <UpdateBeer/>
+</template>
+
+<script>
+import UpdateBeer from '../components/UpdateBeer.vue'
+export default {
+    components: {
+        UpdateBeer
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/views/UpdateBreweryView.vue
@@ -0,0 +1,16 @@
+<template>
+  <UpdateBrewery/>
+</template>
+
+<script>
+import UpdateBrewery from '../components/UpdateBrewery.vue'
+export default {
+    components: {
+        UpdateBrewery
+    }
+}
+</script>
+
+<style>
+
+</style>
